c0f97c7e3a32f795c10db7e59e33391fbc813f70
RicardoJD22 <ricardog_20@alu.uabcs.mx>
2024-05-07 04:43:30 -0700

    Add: Push?

--- clocklp.js
+++ clocklp.js
@@ -5,8 +5,8 @@ const SECOND_HAND_LENGTH = 90;
 const HOUR_HAND_THICKNESS = 4;
 const MINUTE_HAND_THICKNESS = 2;
 const SECOND_HAND_THICKNESS = 1;
-//RELOJ
-//ALGORITMO DE PUNTO MEDIO
+
+//RELOJ LA PAZ
 function drawCircle(xCenter, yCenter, radius) {
   let x = radius;
   let y = 0;
@@ -26,7 +26,7 @@ function drawCircle(xCenter, yCenter, radius) {
       }
   }
 }
-
+//ECUACIÓN PUNTO Y MEDIO PARA CIRCULOS
 function plotPoints(xCenter, yCenter, x, y) {
   
   point(xCenter + x, yCenter + y);
@@ -39,37 +39,69 @@ function plotPoints(xCenter, yCenter, x, y) {
   point(xCenter - y, yCenter - x);
 }
 
+// ALGORITMO ECUACIÓN PUNTO-PENDIENTE
+
+function drawHand(length, angle, thickness, color) {
+    stroke(color);
+    strokeWeight(thickness);
+    let x = length * cos(angle);
+    let y = length * sin(angle);
+    let m = y / x; 
+    let b = 0; 
+  
+    
+    if (abs(x) >= abs(y)) {
+        let startX, endX;
+        if (x >= 0) {
+            startX = 0;
+            endX = x;
+        } else {
+            startX = x;
+            endX = 0;
+        }
+        for (let i = startX; i <= endX; i++) {
+            let xPoint = i;
+            let yPoint = round(m * i + b);
+            point(xPoint, yPoint);
+        }
+    } else { 
+        let startY, endY;
+        if (y >= 0) {
+            startY = 0;
+            endY = y;
+        } else {
+            startY = y;
+            endY = 0;
+        }
+        for (let i = startY; i <= endY; i++) {
+            let yPoint = i;
+            let xPoint = round((yPoint - b) / m);
+            point(xPoint, yPoint);
+        }
+    }
+  }
 
 
 function drawClock(x, y, hora, minuto, segundo) {
-  //ESTRUCTURA DEL RELOJ
+ 
     strokeWeight(2);
     drawCircle(x, y, CLOCK_RADIUS);
 
     push();
     translate(x, y);
     rotate(-90);
-    
-    // HORA
+
     let anguloHora = map(hora * 60, 0, 12 * 60, 0, 360);
     drawHand(HOUR_HAND_LENGTH, anguloHora, HOUR_HAND_THICKNESS, color("black"));
 
-    // MINUTO
+    
     let anguloMinuto = map(minuto * 60, 0, 60 * 60, 0, 360);
     drawHand(MINUTE_HAND_LENGTH, anguloMinuto, MINUTE_HAND_THICKNESS, color("black"));
 
-    // SEGUNDO
     
     drawHand(SECOND_HAND_LENGTH, map(segundo, 0, 60, 0, 360), SECOND_HAND_THICKNESS, color("red"));
 
     pop();
 
     
-}
-
-
-function drawHand(length, angle, thickness, color) {
-    stroke(color);
-    strokeWeight(thickness);
-    line(0, 0, length * cos(angle), length * sin(angle));
 }
